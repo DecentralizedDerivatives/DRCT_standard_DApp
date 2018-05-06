dac6efb12bd7b11b5551d37fac57689ebfa63924
cadidadi <cadidadi@gmail.com>
2018-05-06 11:54:18 -0400

    howto

--- src/ethereum/contracts.js
+++ src/ethereum/contracts.js
@@ -7,8 +7,17 @@ const Factory = contract(factoryArtifact);
 const exchangeArtifact = require('./build/contracts/Exchange.json');
 const Exchange = contract(exchangeArtifact);
 
+const swapArtitifact = require('./build/contracts/TokenToTokenSwap.json');
+const Swap = contract(swapArtitifact);
+
+const wrappedArtifact = require('./build/contracts/Wrapped_Ether.json');
+const Wrapped = contract(wrappedArtifact);
+
+const drctArtifact = require('./build/contracts/DRCT_Token.json');
+const DRCT = contract(drctArtifact);
+
 if (typeof window !== 'undefined' && typeof window.web3 !== 'undefined') {
-  [Factory, Exchange].forEach(Contract =>
+  [Factory,Exchange,Swap,Wrapped,DRCT].forEach(Contract =>
     Contract.setProvider(web3.currentProvider)
   );
   // Factory.at('0xf0836623ef02e8ac937be2d4d3b86386e2e226ef');
@@ -17,4 +26,7 @@ if (typeof window !== 'undefined' && typeof window.web3 !== 'undefined') {
 module.exports = {
   Factory,
   Exchange,
+  Swap,
+  Wrapped,
+  DRCT
 };
