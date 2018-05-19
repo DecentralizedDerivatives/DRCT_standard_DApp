ff563c744158f67632dfc32a837159f88f4c5849
taihelsel <tai.bayard@gmail.com>
2018-05-19 15:50:12 -0700

    added contract details function to portfolio component

--- src/components/Bulletin/Bulletin.js
+++ src/components/Bulletin/Bulletin.js
@@ -1,4 +1,4 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import withStyles from 'material-ui/styles/withStyles';
 import Button from 'material-ui/Button';
@@ -11,13 +11,13 @@ import PriceChart from '../PriceChart';
 import List from '../List';
 import Unlist from '../Unlist';
 import Buy from '../Buy';
-import {Factory, Exchange, web3} from '../../ethereum';
+import { Factory, Exchange, web3 } from '../../ethereum';
 
 class Bulletin extends Component {
-  constructor(){
+  constructor() {
     super();
     this.state = {
-      orderbook: [['loading...', 'loading...', 'loading...', 'loading...','...']],
+      orderbook: [['loading...', 'loading...', 'loading...', 'loading...', '...']],
       previousActive: '',
       recentTrades: [['loading...', 'loading...', 'loading...']],
       active: '',
@@ -26,25 +26,25 @@ class Bulletin extends Component {
       openL: false,
       openB: false,
       orderID: "xxx",
-      myAccount:"xxx",
+      myAccount: "xxx",
       contractAddress: "",
-      contractDuration:"",
-      contractMultiplier:"",
-      oracleAddress:"",
+      contractDuration: "",
+      contractMultiplier: "",
+      oracleAddress: "",
     };
   }
-  fetchData = () => {};
+  fetchData = () => { };
   componentDidMount() {
     web3.eth.getAccounts((error, accounts) => {
-      this.setState({myAccount: accounts[0]});
+      this.setState({ myAccount: accounts[0] });
     });
     this.getOrderBook().then(result => {
-      this.setState({orderbook: result});
+      this.setState({ orderbook: result });
     });
     this.getRecentTrades().then(res => {
-            this.setState({recentTrades: res});
+      this.setState({ recentTrades: res });
     });
-    //Gettings contract details one time when its parent gets mounted
+    //Getting contract details one time when its parent gets mounted
     this.getContractDetails();
   }
 
@@ -58,7 +58,7 @@ class Bulletin extends Component {
 
   onClickRow = link => {
     this.openContractDetails();
-    this.setState({active: link});
+    this.setState({ active: link });
   };
 
   onBuyClick = link => {
@@ -67,7 +67,7 @@ class Bulletin extends Component {
 
   openContractDetails = () => {
     this.setState({
-      open: true, 
+      open: true,
       previousActive: this.state.active,
     });
   };
@@ -90,7 +90,6 @@ class Bulletin extends Component {
       oracleAddress: response[3],
     });
   };
-
   closeContractDetails = () => {
     this.setState({
       open: false,
@@ -99,9 +98,9 @@ class Bulletin extends Component {
   };
 
   openBuy = (link) => {
-     console.log('Link',link);
-    this.setState({orderID:link})
-    this.setState({openB: true, previousActive: this.state.active});
+    console.log('Link', link);
+    this.setState({ orderID: link })
+    this.setState({ openB: true, previousActive: this.state.active });
   };
 
   closeBuy = () => {
@@ -114,7 +113,7 @@ class Bulletin extends Component {
   };
 
   openList = () => {
-    this.setState({openL: true, previousActive: this.state.active});
+    this.setState({ openL: true, previousActive: this.state.active });
   };
 
   closeList = () => {
@@ -127,7 +126,7 @@ class Bulletin extends Component {
   };
 
   openUnlist = () => {
-    this.setState({openU: true, previousActive: this.state.active});
+    this.setState({ openU: true, previousActive: this.state.active });
   };
 
   closeUnlist = () => {
@@ -139,7 +138,7 @@ class Bulletin extends Component {
     this.getRecentTrades();
   };
 
-  buyOrder = () => {};
+  buyOrder = () => { };
 
   getRecentTrades = async () => {
     const exchange = await Exchange.deployed();
@@ -147,20 +146,20 @@ class Bulletin extends Component {
 
     let transferEvent = await exchange.Sale(
       {},
-      {fromBlock: 0, toBlock: 'latest'}
+      { fromBlock: 0, toBlock: 'latest' }
     );
 
     await transferEvent.get((error, logs) => {
       console.log(logs.length);
       for (let i = logs.length - 1; i >= Math.max(logs.length - 10, 0); i--) {
-        _trades.push([logs[i].args['_token'].toString(),logs[i].args['_amount'].toString(),logs[i].args['_price'].toString()]);
+        _trades.push([logs[i].args['_token'].toString(), logs[i].args['_amount'].toString(), logs[i].args['_price'].toString()]);
       }
       if (logs.length === 0) {
         console.log('setting');
         _trades = [['No Recent Trades', '...', '...']];
       }
     });
-            return _trades;
+    return _trades;
   };
 
   getOrderBook = async () => {
@@ -180,13 +179,13 @@ class Bulletin extends Component {
       let book = await exchange.openBooks(i);
       let orders = await exchange.getOrders(book);
 
-      for (let j=0; j<orders.length;j++) {
-        if(orders[j].c[0] > 0){
+      for (let j = 0; j < orders.length; j++) {
+        if (orders[j].c[0] > 0) {
           order = await exchange.getOrder(orders[j].c[0]);
           var _date = await factory.token_dates.call(book);
           _date = new Date(_date * 1000);
-          _date = (_date.getMonth()+1) + '/' + _date.getDate() + '/' + _date.getFullYear() 
-          o_row = [orders[j].c[0].toString(),order[3],(order[1].c[0]/10000).toString(),order[2].c[0].toString(),_date.toString()];
+          _date = (_date.getMonth() + 1) + '/' + _date.getDate() + '/' + _date.getFullYear()
+          o_row = [orders[j].c[0].toString(), order[3], (order[1].c[0] / 10000).toString(), order[2].c[0].toString(), _date.toString()];
           allrows.push(o_row);
         }
       }
@@ -196,7 +195,7 @@ class Bulletin extends Component {
   };
 
   render() {
-    const {classes} = this.props;
+    const { classes } = this.props;
 
     return (
       <Grid
@@ -257,22 +256,22 @@ class Bulletin extends Component {
           contractDuration={this.state.contractDuration}
           contractMultiplier={this.state.contractMultiplier}
           oracleAddress={this.state.oracleAddress}
-      />
-     <List
+        />
+        <List
           open={this.state.openL}
           toggle={this.closeList}
-      />      
+        />
         <Unlist
-          myAccount ={this.state.myAccount}
+          myAccount={this.state.myAccount}
           open={this.state.openU}
           toggle={this.closeUnlist}
-      />
+        />
 
-      <Buy
-          orderID = {this.state.orderID}
+        <Buy
+          orderID={this.state.orderID}
           open={this.state.openB}
           toggle={this.closeBuy}
-      />
+        />
 
 
       </Grid>
