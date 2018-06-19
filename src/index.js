42ad1fa00c471af6ce4fb2b323af384d820863b7
rayGreycloud <rayGreycloud@gmail.com>
2018-06-18 18:34:49 -0700

    Add materialize-css

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {BrowserRouter} from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import Main from './components/Main';
 import 'normalize.css';
+import 'materialize-css/dist/css/materialize.min.css';
 
 ReactDOM.render(
   <BrowserRouter>
