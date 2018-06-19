924b5276868cfccd17ee48d64d587bc7b42e260f
rayGreycloud <rayGreycloud@gmail.com>
2018-06-18 18:24:18 -0700

    Add TextField component to replace material-ui component

---
+++ src/components/TextField/TextField.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import './textFieldStyles.css';
+
+const TextField = ({
+  id,
+  type,
+  className,
+  placeholder,
+  label,
+  name,
+  value,
+  onChange,
+  disabled,
+  helperText
+}) => {
+  return (
+    <div className="input-field">
+      <input
+        id={id}
+        type={type}
+        className={className}
+        placeholder={placeholder}
+        name={name}
+        value={value}
+        onChange={onChange}
+        disabled={disabled}
+      />
+      <label for={id}>{label}</label>
+      <span class="helper-text" data-error="wrong" data-success="right">
+        {helperText}
+      </span>
+    </div>
+  );
+};
+
+TextField.propTypes = {
+  id: PropTypes.string.isRequired,
+  type: PropTypes.string.isRequired,
+  className: PropTypes.string,
+  placeholder: PropTypes.string,
+  label: propTypes.string,
+  name: PropTypes.string.isRequired,
+  value: PropTypes.string.isRequired,
+  onChange: PropTypes.func.isRequired,
+  disabled: PropTypes.string,
+  helperText: PropTypes.string
+};
+
+TextField.defaultProps = {
+  type: 'text'
+};
+
+export default TextField;
