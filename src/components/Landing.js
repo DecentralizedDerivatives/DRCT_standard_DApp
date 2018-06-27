8f088e28c1111bdb3b5660009415f8fad8538bcb
rayGreycloud <rayGreycloud@gmail.com>
2018-06-26 19:35:09 -0700

    Add Landing page

---
+++ src/components/Landing.js
@@ -0,0 +1,46 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import { PropTypes } from 'prop-types';
+import { connect } from 'react-redux';
+
+class Landing extends Component {
+  componentDidMount() {
+    if (this.props.connected) {
+      this.props.history.push('/portfolio');
+    }
+  }
+
+  render() {
+    return (
+      <div className="landing">
+        <div className="dark-overlay landing-inner text-light">
+          <div className="container">
+            <div className="row">
+              <div className="col-md-12 text-center">
+                <h1 className="display-3 mb-4" />
+                <p className="lead"> </p>
+                <hr />
+                <Link to="/register" className="btn btn-lg btn-info mr-2">
+                  Sign Up
+                </Link>
+                <Link to="/login" className="btn btn-lg btn-light">
+                  Login
+                </Link>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+Landing.propTypes = {
+  connected: PropTypes.bool.isRequired
+};
+
+const mapStateToProps = state => ({
+  connected: state.connected
+});
+
+export default connect(mapStateToProps)(Landing);
