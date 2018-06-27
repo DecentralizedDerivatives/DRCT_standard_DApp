5165d3ed5d513743da872b6f2a7cedb6440627fc
rayGreycloud <rayGreycloud@gmail.com>
2018-06-26 19:29:42 -0700

    Add components refactored from Bulletin

---
+++ src/components/OrderBook.js
@@ -0,0 +1,139 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { Table } from 'reactstrap';
+import { Factory, Exchange, web3, DRCT } from '../ethereum';
+import '../styles/orderBook.css';
+
+class OrderBook extends Component {
+  constructor() {
+    super();
+    this.state = {
+      previousActive: '',
+      active: '',
+      orderbook: [
+        ['loading...', 'loading...', 'loading...', 'loading...', '...']
+      ],
+      myAccount: '',
+      contractAddress: '',
+      contractDuration: '',
+      contractMultiplier: ''
+    };
+  }
+
+  componentDidMount() {
+    web3.eth.getAccounts((error, accounts) => {
+      this.setState({ myAccount: accounts[0] });
+    });
+
+    this.getOrderBook().then(result => {
+      this.setState({ orderbook: result });
+    });
+  }
+
+  getOrderBook = async () => {
+    const factory = await Factory.at(
+      '0x15bd4d9dd2dfc5e01801be8ed17392d8404f9642'
+    );
+    //orderbook
+
+    // first get number of open books (tokens with open orders):
+    let exchange = await Exchange.deployed();
+    let numBooks = await exchange.getBookCount();
+
+    // get orders for that book:
+    let allrows = [];
+    let order;
+    for (let i = 0; i < numBooks; i++) {
+      let book = await exchange.openBooks(i);
+      let orders = await exchange.getOrders(book);
+      for (let j = 0; j < orders.length; j++) {
+        if (orders[j].c[0] > 0) {
+          order = await exchange.getOrder(orders[j].c[0]);
+          let _date = await factory.token_dates.call(book);
+          _date = new Date(_date * 1000);
+          _date =
+            _date.getUTCMonth() +
+            1 +
+            '/' +
+            _date.getUTCDate() +
+            '/' +
+            _date.getUTCFullYear();
+          allrows.push({
+            orderId: orders[j].c[0].toString(),
+            address: order[3],
+            price: (order[1].c[0] / 10000).toString(),
+            quantity: order[2].c[0].toString(),
+            date: _date.toString(),
+            contractDuration: this.state.contractDuration,
+            contractMultiplier: this.state.contractMultiplier,
+            symbol: 'BTC/USD' /*CURRENTLY USING STATIC SYMBOL NEED TO FIX*/
+          });
+        }
+      }
+    }
+    return allrows;
+  };
+
+  renderRows() {
+    this.state.orderbook.map(order => {
+      const {
+        orderId,
+        address,
+        price,
+        quantity,
+        date,
+        contractDuration,
+        contractMultiplier,
+        symbol
+      } = order;
+
+      return (
+        <tr>
+          <td>{orderId}</td>
+          <td>
+            <a
+              className="token-address-link"
+              onClick={e => e.stopPropagation()}
+              data-token-address={address}
+            >
+              <span>
+                {symbol} - {contractDuration} Days - {contractMultiplier}X
+              </span>
+            </a>
+          </td>
+          <td>{price}</td>
+          <td>{quantity}</td>
+          <td>{date}</td>
+        </tr>
+      );
+    });
+  }
+
+  render() {
+    const { Body, Header, HeaderCell, Row } = Table;
+
+    return (
+      <div className="container">
+        <div className="order-book">
+          <Table className="positions-table" onClick={this.onClickRow}>
+            <thead>
+              <tr>
+                <th>Order Book</th>
+              </tr>
+              <tr>
+                <th>Order Id</th>
+                <th>Asset</th>
+                <th>Price (ETH)</th>
+                <th>Quantity</th>
+                <th>Start Date</th>
+              </tr>
+            </thead>
+            <tbody>{this.renderRows()}</tbody>
+          </Table>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default OrderBook;
---
+++ src/components/RecentTrades.js
@@ -0,0 +1,125 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { Table } from 'reactstrap';
+import { Factory, Exchange, web3, DRCT } from '../ethereum';
+import '../styles/recentTrades.css';
+
+class RecentTrades extends Component {
+  constructor() {
+    super();
+    this.state = {
+      previousActive: '',
+      active: '',
+      myAccount: '',
+      selectedTokenAddress: '',
+      contractAddress: '',
+      contractDuration: '',
+      contractMultiplier: '',
+      oracleAddress: '',
+      recentTrades: ''
+    };
+  }
+
+  componentDidMount() {
+    web3.eth.getAccounts((error, accounts) => {
+      this.setState({ myAccount: accounts[0] });
+    });
+
+    this.getRecentTrades().then(res => {
+      this.setState({ recentTrades: res });
+    });
+  }
+
+  getRecentTrades = async () => {
+    const exchange = await Exchange.deployed();
+    var _trades = [];
+
+    let transferEvent = await exchange.Sale(
+      {},
+      { fromBlock: 0, toBlock: 'latest' }
+    );
+
+    await transferEvent.get((error, logs) => {
+      for (let i = logs.length - 1; i >= Math.max(logs.length - 10, 0); i--) {
+        _trades.push({
+          address: logs[i].args['_token'].toString(),
+          volume: logs[i].args['_amount'].toString(),
+          price: (logs[i].args['_price'] / 1e18).toString(),
+          contractDuration: this.state.contractDuration,
+          contractMultiplier: this.state.contractMultiplier,
+          symbol: 'BTC/USD' /*CURRENTLY USING STATIC SYMBOL NEED TO FIX*/
+        });
+      }
+      if (logs.length === 0) {
+        _trades = [['No Recent Trades', '...', '...']];
+      }
+    });
+    return _trades;
+  };
+
+  onClickRow = link => {
+    let addressEl = link.currentTarget.getElementsByClassName(
+      'token-address-link'
+    )[0];
+    if (typeof addressEl !== 'undefined') {
+      // Same API for recent trades details?
+      // this.openContractDetails(
+      //   link,
+      //   addressEl.getAttribute('data-token-address')
+      // );
+    }
+  };
+
+  renderRows() {
+    this.state.recentTrades.map(trade => {
+      const {
+        address,
+        volume,
+        price,
+        contractDuration,
+        contractMultiplier,
+        symbol
+      } = trade;
+
+      return (
+        <tr>
+          <td>
+            <a
+              className="token-address-link"
+              onClick={event => event.stopPropagation()}
+              data-token-address={address}
+            >
+              {symbol} - {contractDuration} Days - {contractMultiplier}X
+            </a>
+          </td>
+          <td>{volume}</td>
+          <td>{price}</td>
+        </tr>
+      );
+    });
+  }
+
+  render() {
+    const { Body, Header, HeaderCell, Row } = Table;
+
+    return (
+      <div className="flex-item">
+        <Table className="recent-trades-table" onClick={this.onRowClick}>
+          <thead>
+            <tr>
+              <th>Recent Trades</th>
+            </tr>
+            <tr>
+              <th>Address</th>
+              <th>Volume</th>
+              <th>Price (ETH)</th>
+            </tr>
+          </thead>
+          <tbody>{this.renderRows()}</tbody>
+        </Table>
+      </div>
+    );
+  }
+}
+
+export default RecentTrades;
