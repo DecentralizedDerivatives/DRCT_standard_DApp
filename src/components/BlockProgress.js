8fcef23efab29796f575629184b5de97b3231c9a
rayGreycloud <rayGreycloud@gmail.com>
2018-06-26 18:53:58 -0700

    Add new processing spinner

---
+++ src/components/BlockProgress.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import '../styles/blockProgress.css';
+
+const BlockProgress = () => (
+  <div style="width:100%;height:0;padding-bottom:100%;position:relative;">
+    <iframe
+      src="https://giphy.com/embed/K5kfQExKk731K"
+      width="100%"
+      height="100%"
+      style="position:absolute"
+      frameBorder="0"
+      class="giphy-embed"
+      allowFullScreen
+    />
+  </div>
+);
+
+export default BlockProgress;
