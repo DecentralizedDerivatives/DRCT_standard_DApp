a431e26935d805bddc4156060c88b8695232e8a2
rayGreycloud <rayGreycloud@gmail.com>
2018-06-26 19:17:23 -0700

    Refactor Header and navigation

---
+++ src/components/Header.js
@@ -0,0 +1,106 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+  UncontrolledDropdown,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem
+} from 'reactstrap';
+import '../styles/header.css';
+
+class Header extends Component {
+  constructor(props) {
+    super(props);
+
+    this.toggle = this.toggle.bind(this);
+    this.state = {
+      isOpen: false
+    };
+  }
+
+  toggle() {
+    this.setState({
+      isOpen: !this.state.isOpen
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <Navbar className="bg-dark" dark expand="md">
+          <NavbarBrand href="/">
+            <img
+              src="../imgs/dda-logo.png"
+              alt="logo"
+              height="25px"
+              width="25px"
+            />DRCT
+          </NavbarBrand>
+          <NavbarToggler onClick={this.toggle} />
+          <Collapse isOpen={this.state.isOpen} navbar>
+            <Nav className="ml-auto" navbar>
+              <NavItem>
+                <NavLink href="/portfolio/">Portfolio</NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink href="/how-to/">How To</NavLink>
+              </NavItem>
+              <UncontrolledDropdown nav inNavbar>
+                <DropdownToggle nav caret>
+                  More
+                </DropdownToggle>
+                <DropdownMenu right>
+                  <DropdownItem>
+                    <NavLink href="/buy/">Buy</NavLink>
+                  </DropdownItem>
+                  <DropdownItem>
+                    <NavLink href="/list/">List</NavLink>
+                  </DropdownItem>
+                  <DropdownItem>
+                    <NavLink href="/unlist/">Unlist</NavLink>
+                  </DropdownItem>
+                  <DropdownItem divider />
+                  <DropdownItem>
+                    <NavLink href="/create-contract/">Create Contract</NavLink>
+                  </DropdownItem>
+                </DropdownMenu>
+              </UncontrolledDropdown>
+              <NavItem>
+                <span>
+                  Connected
+                  <i
+                    className={classnames('fa-circle', {
+                      fas: this.props.connected,
+                      far: !this.props.connected
+                    })}
+                  />
+                </span>
+                />
+              </NavItem>
+            </Nav>
+          </Collapse>
+        </Navbar>
+      </div>
+    );
+  }
+}
+
+Header.propTypes = {
+  connected: PropTypes.bool.isRequired
+};
+
+Navbar.propTypes = {
+  light: PropTypes.bool,
+  dark: PropTypes.bool,
+  color: PropTypes.string,
+  expand: PropTypes.string
+};
+
+export default Header;
