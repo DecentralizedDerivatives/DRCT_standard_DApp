292cae7e3ddc363810971a168ca9a8321b786429
rayGreycloud <rayGreycloud@gmail.com>
2018-06-27 17:38:45 -0700

    Combine highchart options into PriceChart

--- src/components/PriceChart.js
+++ src/components/PriceChart.js
@@ -6,7 +6,69 @@ require('highcharts/modules/exporting')(Highcharts);
 import highchartsOptions from './highcharts.options';
 import '../styles/pricechart.css';
 
-Highcharts.setOptions(highchartsOptions);
+Highcharts.setOptions({
+  rangeSelector: {
+    inputEnabled: false,
+    align: 'right',
+    buttons: [
+      {
+        type: 'month',
+        count: 1,
+        text: '1m'
+      },
+      {
+        type: 'all',
+        count: 3,
+        text: '3m'
+      }
+    ]
+  },
+
+  exporting: {
+    enabled: false
+  },
+
+  navigator: {
+    enabled: false
+  },
+
+  colors: [
+    '#30AD63' //line
+  ],
+  chart: {
+    backgroundColor: '#EEF2F5',
+    style: {
+      fontFamily: "'Unica One', sans-serif"
+    }
+  },
+  subtitle: {
+    style: {
+      color: '#E0E0E3',
+      textTransform: 'uppercase'
+    }
+  },
+  xAxis: {
+    gridLineWidth: 0,
+    minorGridLineWidth: 0,
+    tickWidth: 0,
+    lineWidth: 0,
+    labels: {
+      enabled: false
+    }
+  },
+  yAxis: {
+    gridLineColor: 'white',
+    labels: {
+      align: 'left',
+      style: {
+        color: 'gray'
+      }
+    }
+  },
+  scrollbar: {
+    enabled: false
+  }
+});
 
 class PriceChart extends Component {
   constructor() {
@@ -15,17 +77,21 @@ class PriceChart extends Component {
       currency: 'BTC'
     };
   }
+
   static currencies = ['BTC', 'ETH'];
 
   componentDidMount() {
     this.fetchData('btc');
   }
+
+  // TODO:Move into action
   fetchData = async type => {
     const data = await api[type].get();
     this.setState({ data }, () => {
       this.createChart();
     });
   };
+
   createChart = type => {
     Highcharts.stockChart('container', {
       series: [
