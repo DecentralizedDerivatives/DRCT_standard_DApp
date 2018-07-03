bfaf11a0c9fe4211082ec5f013eaf6ea19d000a6
rayGreycloud <rayGreycloud@gmail.com>
2018-07-03 16:05:06 -0700

    Place block spinner in modal, show when processing

--- src/components/BlockProgress.js
+++ src/components/BlockProgress.js
@@ -1,24 +1,44 @@
-import React from 'react';
-import { Card, CardBody, CardFooter, CardImg, CardHeader } from 'reactstrap';
+import React, { Component } from 'react';
+import {
+  Card,
+  CardBody,
+  CardFooter,
+  CardImg,
+  CardHeader,
+  Modal
+} from 'reactstrap';
 
-const BlockProgress = () => (
-  <Card>
-    <CardHeader>Processing...</CardHeader>
-    <CardBody>
-      <CardImg
-        top
-        tag="iframe"
-        width="100%"
-        frameBorder="0"
-        src="https://giphy.com/embed/K5kfQExKk731K"
-        alt="Card image cap"
-      />
-    </CardBody>
-    <CardFooter>This could take several minutes...</CardFooter>
-  </Card>
+class BlockProgress extends Component {
+  render() {
+    return (
+      <Modal isOpen={this.props.isProcessing}>
+        <Card>
+          <CardHeader className="text-center">Processing...</CardHeader>
+          <CardBody>
+            <CardImg
+              top
+              tag="iframe"
+              width="100%"
+              frameBorder="0"
+              src="https://giphy.com/embed/K5kfQExKk731K"
+              alt="Card image cap"
+            />
+          </CardBody>
+          <CardFooter className="text-center">
+            This could take several minutes...
+          </CardFooter>
+        </Card>
+      </Modal>
+    );
+  }
+}
 
-  // Small gif without iframe
-  // <img src="https://media.giphy.com/media/K5kfQExKk731K/200w_d.gif" alt="">
-);
+ConnectionModal.propTypes = {
+  isProcessing: PropTypes.bool.isRequired
+};
 
-export default BlockProgress;
+const mapStateToProps = state => ({
+  isProcessing: state.status.isProcessing
+});
+
+export default connect(mapStateToProps)(BlockProgress);
