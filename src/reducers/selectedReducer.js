ba7427a6c7e377d943de6a727e71080a2ac64f0b
rayGreycloud <rayGreycloud@gmail.com>
2018-07-02 18:33:01 -0700

    Add selected actions/reducers

---
+++ src/reducers/selectedReducer.js
@@ -0,0 +1,30 @@
+import { SET_CURRENT, SET_SELECTED_TOKEN, CLEAR_SELECTED_TOKEN } from './types';
+
+const initialState = {
+  selectedToken: '',
+  selectedOrderID: ''
+};
+
+export default function(state = initialState, action) {
+  switch (action.type) {
+    case SET_CURRENT:
+      const { selectedToken, selectedOrderID } = action.payload;
+      return {
+        ...state,
+        selectedToken,
+        selectedOrderID
+      };
+    case SET_SELECTED_TOKEN:
+      return {
+        ...state,
+        selectedToken: action.payload
+      };
+    case CLEAR_SELECTED_TOKEN:
+      return {
+        ...state,
+        selectedToken: ''
+      };
+    default:
+      return state;
+  }
+}
