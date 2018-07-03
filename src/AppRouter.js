5498ef920fd82dd5b7d9dce8a950149eefc2dd01
rayGreycloud <rayGreycloud@gmail.com>
2018-07-03 16:07:05 -0700

    Add processing and connection modals

--- src/AppRouter.js
+++ src/AppRouter.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Switch, Route, Redirect } from 'react-router-dom';
-import ErrorModal from './components/ErrorModal';
+import ConnectionModal from './components/ConnectionModal';
+import BlockProgress from './components/BlockProgress';
 import Header from './components/Header';
 import Landing from './components/Landing';
 import MyPortfolio from './components/MyPortfolio';
@@ -8,37 +9,13 @@ import Bulletin from './components/Bulletin';
 import CashOut from './components/CashOut';
 import HowTo from './components/HowTo';
 import { web3 } from '../ethereum';
-import '../styles/main.css';
+import { checkUserConnection } from '../actions/statusActions';
+import '../styles/AppRouter.css';
 
 class AppRouter extends Component {
-  constructor() {
-    super();
-    this.state = {
-      modal: false,
-      accounts: [],
-      network: '',
-      connected: true
-    };
-  }
   async componentDidMount() {
-    const accounts = await web3.eth.getAccounts();
-    const network = await web3.eth.net.getId();
-    if (!accounts.length || network !== 4) {
-      this.setState({ modal: true, connected: false, accounts, network });
-    } else {
-      this.setState({ accounts, network });
-    }
+    await this.props.checkUserConnection();
   }
-  onModalPress = () => this.setState({ modal: !this.state.modal });
-
-  // Convert to middleware
-  isWeb3Active = component => {
-    const { accounts, network } = this.state;
-    if (network !== 4 || !accounts.length) {
-      return null;
-    }
-    return component;
-  };
 
   render() {
     return (
@@ -46,16 +23,20 @@ class AppRouter extends Component {
         <Header connected={this.state.connected} />
         <Route exact path="/" component={Landing} />
         <Switch>
-          <Route path="/portfolio" component={this.isWeb3Active(MyPortfolio)} />
-          <Route path="/bulletin" component={this.isWeb3Active(Bulletin)} />
+          <Route path="/portfolio" component={MyPortfolio} />
+          <Route path="/bulletin" component={Bulletin} />
           <Route path="/how-to" component={HowTo} />
           <Redirect to="/" />
         </Switch>
 
-        <ErrorModal open={this.state.modal} onClick={this.onModalPress} />
+        <BlockProgress />
+        <ConnectionModal />
       </div>
     );
   }
 }
 
-export default AppRouter;
+export default connect(
+  null,
+  { checkUserConnection }
+)(AppRouter);
---
+++ src/components/ConnectionModal.js
@@ -0,0 +1,58 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import { Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
+import { Link } from 'react-router-dom';
+import '../styles/ConnectionModal.css';
+
+// Refactor as class component
+class ConnectionModal extends Component {
+  constructor(props) {
+    super(props);
+
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick() {
+    this.props.showConnectionModal(false);
+    this.props.history.push('/');
+  }
+
+  render() {
+    return (
+      <Modal isOpen={this.props.isConnectModalOpen}>
+        <ModalHeader id="alert-dialog-title">
+          Unable to detect network
+        </ModalHeader>
+        <ModalBody>
+          <div id="alert-dialog-description">
+            <span>
+              Are you sure you're connected to the Ethereum Rinkeby Testnet?
+            </span>
+          </div>
+        </ModalBody>
+        <ModalFooter>
+          <button className="btn btn-primary" onClick={this.handleClick}>
+            <Link className="button-link" to="/">
+              Homepage
+            </Link>
+          </button>
+        </ModalFooter>
+      </Modal>
+    );
+  }
+}
+
+ConnectionModal.propTypes = {
+  showConnectionModal: PropTypes.func.isRequired,
+  isConnectModalOpen: PropTypes.bool.isRequired
+};
+
+const mapStateToProps = state => ({
+  isConnectModalOpen: state.status.isConnectModalOpen
+});
+
+export default connect(
+  mapStateToProps,
+  { showConnectionModal }
+)(ConnectionModal);
