1c68fdd51904008fd623beb3ace85352170e833c
rayGreycloud <rayGreycloud@gmail.com>
2018-07-06 13:25:47 -0700

    Refactor display of landing page message and buttons

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -4,6 +4,78 @@ import { connect } from 'react-redux';
 import { PropTypes } from 'prop-types';
 
 class Landing extends Component {
+  renderMessage() {
+    let message;
+    const network = 'Rinkeby Test Network';
+
+    if (!this.props.metamask) {
+      message = (
+        <div>
+          <p>Please Login to MetaMask</p>
+          <p>Select: {network}</p>
+        </div>
+      );
+    } else if (this.props.metamask && this.props.network !== 4) {
+      message = (
+        <div>
+          <p>Please Set MetaMask To:</p>
+          <p>{network}</p>
+        </div>
+      );
+    } else if (this.props.metamask && this.props.network === 4) {
+      message = (
+        <div>
+          <p>You are Connected To:</p>
+          <p>
+            <em>{network}</em>
+          </p>
+        </div>
+      );
+    }
+
+    return <div className="lead">{message}</div>;
+  }
+
+  renderButtons() {
+    let msgBtn = '';
+
+    const ddacoopBtn = (
+      <a
+        href="http://wwww.ddacoop.org"
+        className="btn btn-info ml-2"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Learn More About DRCT DApp
+      </a>
+    );
+
+    if (!this.props.metamask) {
+      msgBtn = (
+        <a
+          className="btn btn-primary mr-2"
+          href="https://metamask.io/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Need MetaMask?
+        </a>
+      );
+    } else if (this.props.metamask && this.props.network === 4) {
+      msgBtn = (
+        <Link to="/portfolio/" className="btn btn-success link mr-2">
+          Go to My Portfolio
+        </Link>
+      );
+    }
+
+    return (
+      <div className="center-items">
+        {msgBtn} {ddacoopBtn}
+      </div>
+    );
+  }
+
   render() {
     return (
       <div className="landing__bg">
@@ -12,56 +84,9 @@ class Landing extends Component {
             <div className="row">
               <div className="col-md-12 text-center">
                 <h1 className="display-3 mb-4">DRCT DApp</h1>
-                {!this.props.metamask && (
-                  <div>
-                    <p className="lead">
-                      Please log into your MetaMask extension to access this
-                      site.
-                    </p>
-                    <hr />
-                    <a
-                      className="btn btn-lg btn-light"
-                      href="https://metamask.io/"
-                      target="_blank"
-                      rel="noopener noreferrer"
-                    >
-                      Need MetaMask?
-                    </a>
-                  </div>
-                )}
-
-                {this.props.metamask &&
-                  this.props.network !== 4 && (
-                    <div>
-                      <p className="lead">
-                        Please make sure MetaMask is set to{' '}
-                        <em>Rinkeby Test Network</em>
-                      </p>
-                    </div>
-                  )}
-
-                {this.props.metamask &&
-                  this.props.network === 4 && (
-                    <div>
-                      <p className="lead">
-                        You are connected to <em>Rinkeby Test Network</em>
-                      </p>
-                      <button>
-                        <Link href="/portfolio/">Go to My Portfolio</Link>
-                      </button>
-                    </div>
-                  )}
-
-                <div>
-                  <a
-                    href="http://wwww.ddacoop.org"
-                    className="btn btn-lg btn-info mr-2"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                  >
-                    Learn More About DRCT DApp
-                  </a>
-                </div>
+                {this.renderMessage()}
+                <hr />
+                {this.renderButtons()}
               </div>
             </div>
           </div>
