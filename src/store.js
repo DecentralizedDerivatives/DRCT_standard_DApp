678e040c47482e2e1711ce7ea54a750bc7b2add2
Dan Heckman <dan@gainsboroughtech.com>
2018-07-10 21:23:50 -0400

    Compilation fix for redux store

--- src/store.js
+++ src/store.js
@@ -1,17 +1,16 @@
 import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
+import { createLogger } from 'redux-logger'
 import rootReducer from './reducers';
-const middleware = [thunk];
 
 const initialState = {};
+const middlewares = [thunk];
+let composeEnhancer = compose;
 
-const store = createStore(
-  rootReducer,
-  initialState,
-  compose(
-    applyMiddleware(...middleware),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-  )
-);
+if (process.env.NODE_ENV === 'development') {
+  middlewares.push(createLogger());
+  composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+}
+const store = createStore(rootReducer, initialState, composeEnhancer(applyMiddleware(...middlewares)));
 
 export default store;
