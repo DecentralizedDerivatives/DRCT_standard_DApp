f530927144849c4e9948671758f9467adeed328c
Dan Heckman <dan@gainsboroughtech.com>
2018-07-30 21:58:21 -0400

    Fix button styling on Portfolio page

--- src/components/CashOutFormContainer.js
+++ src/components/CashOutFormContainer.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
-import { reduxForm,change } from 'redux-form';
+import { reduxForm } from 'redux-form';
 import { sendCashOutRequest } from '../actions/userActions';
 import CashOutFormComponent from './CashOutFormComponent';
 
