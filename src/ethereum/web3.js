6e9c98a6e057cc359e833f83534d224891bbe55a
themandalore <fett.nicholas@yahoo.com>
2018-09-03 16:25:17 -0400

    web3

--- src/ethereum/web3.js
+++ src/ethereum/web3.js
@@ -5,11 +5,7 @@ let web3;
 if (typeof window !== 'undefined' && typeof window.web3 !== 'undefined') {
   web3 = new Web3(window.web3.currentProvider);
 } else {
-  window.addEventListener('message', ({ data }) => {
-    if (data && data.type && data.type === 'ETHEREUM_PROVIDER_SUCCESS') {
-      web3 = new Web3(new Web3.providers.HttpProvider('http://localhost:8545'));
-    }
-  })
+  web3 = new Web3(new Web3.providers.HttpProvider('http://localhost:8545'));
 }
 
 module.exports = web3;
