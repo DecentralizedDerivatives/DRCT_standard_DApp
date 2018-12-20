f36bed169d0e1bea360fdc7e305ad14a1bd80301
SamuelLJackson <severett29@gmail.com>
2018-12-19 22:41:16 -0500

    fix server.js

--- server.js
+++ server.js
@@ -6,7 +6,8 @@ const port = process.env.PORT || 3000;
 
 const env = {
   'NETWORK_ID': process.env.NETWORK_ID || 4,
-  'API_KEY': process.env.API_KEY || 0,
+  //'API_KEY': process.env.API_KEY || 0,
+  'API_KEY': 0,
   'ALCHEMY': process.env.ALCHEMY || 0,
   }
 
